bc301c777808ca689a159fc848fa74699e7fcd52
Lílian Rodrigues <41344283+RodriguesLilian@users.noreply.github.com>
2024-05-22 17:31:17 -0300

    feature: add transparency

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import cv2
 import os
+import asyncio
+import cv2
+import numpy # cv2
 import pygame
 import random
 
-WHITE = (255, 255, 255)  # Example color, define if not already defined
-WINDOW_WIDTH = 1280  # Define window width if not already defined
-FPS = 30  # Define FPS if not already defined
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 860
+FPS = 30
 
 current_directory = os.path.dirname(__file__)
 
@@ -15,20 +19,17 @@ cats_directory = os.path.join(images_directory, 'cats')
 sounds_directory = os.path.join(current_directory, 'sounds')
 video_directory = os.path.join(current_directory, 'video')
 
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, filename, x, y):
         super().__init__()
-
-        # Get the image name without extension
         self.name = filename.split('.')[0]
-        
         self.original_image = pygame.image.load(os.path.join(cats_directory, filename))
-        self.back_image = pygame.Surface((128, 128))  # Create a surface for back image
-        self.back_image.fill(WHITE)  # Fill back image surface with white color
-
+        self.back_image = pygame.Surface((128, 128))
+        self.back_image.fill(WHITE)
+        self.back_image.set_alpha(128)
         self.image = self.back_image
         self.rect = self.image.get_rect(topleft=(x, y))
-        # Hiding the image
         self.shown = False
 
     def update(self):
@@ -40,19 +41,14 @@ class Tile(pygame.sprite.Sprite):
     def hide(self):
         self.shown = False
 
+
 class Game():
     def __init__(self):
-        # Current game level
         self.level = 1
         self.level_complete = False
-    
-        # Cats
 
-        # List of all cat image files
         self.all_cats = [f for f in os.listdir(cats_directory) if os.path.isfile(os.path.join(cats_directory, f))]
-        
         self.img_width, self.img_height = (128, 128)
-        # Spacing between card images
         self.padding = 20
         self.margin_top = 150
         self.cols = 4
@@ -61,27 +57,19 @@ class Game():
 
         self.tiles_group = pygame.sprite.Group()
 
-        # Flipping and timing
-
         self.flipped = []
-        self.frame_coount = 0
+        self.frame_count = 0
         self.block_game = False
 
-        # Generate first level
-
         self.generate_level(self.level)
-        
-        # Video
 
         self.is_video_playing = True
         self.play = pygame.image.load(os.path.join(images_directory, 'play.png')).convert_alpha()
         self.stop = pygame.image.load(os.path.join(images_directory, 'stop.png')).convert_alpha()
         self.video_toggle = self.play
         self.video_toggle_rect = self.video_toggle.get_rect(topright=(WINDOW_WIDTH - 50, 10))
-        
+
         self.get_video()
-        
-        # Music
 
         self.is_music_playing = True
         self.sound_on = pygame.image.load(os.path.join(images_directory, 'speaker.png')).convert_alpha()
@@ -91,7 +79,7 @@ class Game():
 
         pygame.mixer.music.load(os.path.join(sounds_directory, 'audio.mp3'))
         pygame.mixer.music.set_volume(0.5)
-        pygame.mixer.music.play(loops=-1) 
+        pygame.mixer.music.play(loops=-1)
 
     def update(self, event_list):
         if self.is_video_playing:
@@ -100,7 +88,6 @@ class Game():
         self.user_input(event_list)
         self.draw()
         self.check_level_complete(event_list)
-        #pygame.display.flip()  # Update the display
 
     def check_level_complete(self, event_list):
         if not self.block_game:
@@ -122,15 +109,13 @@ class Game():
                                             self.level_complete = False
                                             break
         else:
-            self.frame_coount += 1
-            if self.frame_coount == FPS:
-                self.frame_coount = 0
+            self.frame_count += 1
+            if self.frame_count == FPS:
+                self.frame_count = 0
                 self.block_game = False
-
                 for tile in self.tiles_group:
                     if tile.name in self.flipped:
                         tile.hide()
-
                 self.flipped = []
 
     def generate_level(self, level):
@@ -141,16 +126,12 @@ class Game():
         self.generate_tileset(self.cats)
 
     def generate_tileset(self, cats):
-        self.cols = self.rows = self.cols if self.cols >= self.rows else self.rows
-
         TILES_WIDTH = (self.img_width * self.cols + self.padding * 3)
-        LEFT_MARGIN = RIGHT_MARGIN = (self.width - TILES_WIDTH) / 2
-        #tiles = []
+        LEFT_MARGIN = (self.width - TILES_WIDTH) / 2
         self.tiles_group.empty()
-
         for i in range(len(cats)):
             x = LEFT_MARGIN + ((self.img_width + self.padding) * (i % self.cols))
-            y = self.margin_top + (i / self.rows * (self.img_height + self.padding))
+            y = self.margin_top + ((i // self.cols) * (self.img_height + self.padding))
             tile = Tile(cats[i], x, y)
             self.tiles_group.add(tile)
 
@@ -160,7 +141,7 @@ class Game():
         cats.extend(cats_copy)
         random.shuffle(cats)
         return cats
-    
+
     def user_input(self, event_list):
         for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
@@ -180,7 +161,6 @@ class Game():
                     else:
                         self.is_video_playing = True
                         self.video_toggle = self.play
-            
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and self.level_complete:
                     self.level += 1
@@ -189,48 +169,43 @@ class Game():
                     self.generate_level(self.level)
 
     def draw(self):
-            screen.fill(BLACK)
+        screen.fill(BLACK)
+        title_font = pygame.font.Font(os.path.join(fonts_directory, 'Sugar Cream.otf'), 44)
+        content_font = pygame.font.Font(os.path.join(fonts_directory, 'Sugar Cream.otf'), 24)
 
-            # Fonts
-            title_font = pygame.font.Font(os.path.join(fonts_directory, 'Sugar Cream.otf'), 44)
-            content_font = pygame.font.Font(os.path.join(fonts_directory, 'Sugar Cream.otf'), 24)
+        title_text = title_font.render('MiXel Memory Game', True, WHITE)
+        title_rect = title_text.get_rect(midtop=(WINDOW_WIDTH / 2, 10))
+        level_text = content_font.render('Level ' + str(self.level), True, WHITE)
+        level_rect = level_text.get_rect(midtop=(WINDOW_WIDTH / 2, 80))
+        info_text = content_font.render('Find 2 of each', True, WHITE)
+        info_rect = info_text.get_rect(midtop=(WINDOW_WIDTH / 2, 120))
 
-            # Text
-            title_text = title_font.render('MiXel Memory Game', True, WHITE)
-            title_rect = title_text.get_rect(midtop=(WINDOW_WIDTH/2, 10))
-            level_text = content_font.render('Level ' + str(self.level), True, WHITE)
-            level_rect = level_text.get_rect(midtop=(WINDOW_WIDTH/2, 80))
-            info_text = content_font.render('Find 2 of each', True, WHITE)
-            info_rect = info_text.get_rect(midtop=(WINDOW_WIDTH/2, 120))
+        screen.blit(title_text, title_rect)
+        screen.blit(level_text, level_rect)
+        screen.blit(info_text, info_rect)
 
-            if self.is_video_playing:
-                if self.success:
-                    screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (50, 150))
-                else:
-                    self.get_video()
+        if self.is_video_playing:
+            if self.success:
+                screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (100, 150))
             else:
-                screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (50, 150))
+                self.get_video()
+        else:
+            screen.blit(pygame.image.frombuffer(self.img.tobytes(), self.shape, 'BGR'), (100, 150))
 
-            if not self.level == 5:
+        if self.level_complete:
+            if self.level != 5:
                 next_text = content_font.render('Level complete. Press space for next level.', True, WHITE)
             else:
                 next_text = content_font.render('Congrats. You won. Press space to play again.', True, WHITE)
-            next_rect = next_text.get_rect(midbottom=(WINDOW_WIDTH/2, WINDOW_HIGHT - 40))
+            next_rect = next_text.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 40))
+            screen.blit(next_text, next_rect)
 
-            pygame.draw.rect(screen, WHITE, (WINDOW_WIDTH - 90, 0, 100, 50))
-            screen.blit(self.music_toggle, self.music_toggle_rect)
-            screen.blit(self.video_toggle, self.video_toggle_rect)
+        pygame.draw.rect(screen, WHITE, (WINDOW_WIDTH - 90, 0, 100, 50))
+        screen.blit(self.music_toggle, self.music_toggle_rect)
+        screen.blit(self.video_toggle, self.video_toggle_rect)
 
-            screen.blit(title_text, title_rect)
-            screen.blit(level_text, level_rect)
-            screen.blit(info_text, info_rect)
-
-            # Draw tileset
-            self.tiles_group.draw(screen)
-            self.tiles_group.update()
-
-            if self.level_complete:
-                screen.blit(next_text, next_rect)
+        self.tiles_group.draw(screen)
+        self.tiles_group.update()
 
     def get_video(self):
         video_path = os.path.join(video_directory, 'video.mp4')
@@ -238,40 +213,25 @@ class Game():
         self.success, self.img = self.cap.read()
         self.shape = self.img.shape[1::-1]
 
+
 pygame.init()
-
-WINDOW_WIDTH = 1200
-WINDOW_HIGHT = 860
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("MiXel Memory Game")
-
-WINDOW_WIDTH = 1200
-WINDOW_HIGHT = 860
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HIGHT))
-pygame.display.set_caption("MiXel Memory Game")
-
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
-
-FPS = 60
 clock = pygame.time.Clock()
 
 game = Game()
 
-# Quit the game
+async def main():
+    running = True
+    while running:
+        event_list = pygame.event.get()
+        for event in event_list:
+            if event.type == pygame.QUIT:
+                running = False
+        game.update(event_list)
+        pygame.display.update()
+        clock.tick(FPS)
+        await asyncio.sleep(0)
 
-running = True
-while running:
-    event_list = pygame.event.get()
-
-    for event in event_list:
-        if event.type == pygame.QUIT:
-            running = False
-
-    game.update(event_list)
-
-    pygame.display.update()
-    clock.tick(FPS)
-
+asyncio.run(main())
 pygame.quit()
